33f7061bce8545f11a1b538e7310e61573d4fbf4
Sean Lang <slang800@gmail.com>
2019-02-24 00:25:37 -0600

    add package setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='unshortener_rpc',
+      version='0.1',
+      description='',
+      author='Sean Lang',
+      author_email='slang800@gmail.com',
+      license='GPL-3.0',
+      packages=['unshortener_rpc'],
+      zip_safe=False)
